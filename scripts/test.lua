b30e2209beaf1c6f95276b2d4753da9e8e8d47d8
Redbeanw44602 <redbeana44945@gmail.com>
2025-04-19 00:15:31 +0800

    tryfix.

--- scripts/test.lua
+++ scripts/test.lua
@@ -263,7 +263,7 @@ end
 
 function get_modified_packages()
     local packages = {}
-    local diff = os.iorun("git --no-pager diff HEAD~1")
+    local diff = os.iorun("git --no-pager diff HEAD^")
     for _, line in ipairs(diff:split("\n")) do
         if line:startswith("+++ b/") then
             local file = line:sub(7)
