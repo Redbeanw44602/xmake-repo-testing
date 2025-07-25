1f642398d4688a36740aa3cec51a7a2805f82dba
Redbeanw44602 <redbeana44945@gmail.com>
2025-07-26 04:52:40 +0800

    fix.

--- packages/l/libzchunk/xmake.lua
+++ packages/l/libzchunk/xmake.lua
@@ -8,11 +8,25 @@ package("libzchunk")
 
     add_versions("1.5.1", "2c187055e2206e62cef4559845e7c2ec6ec5a07ce1e0a6044e4342e0c5d7771d")
 
+    add_patches("<=1.5.1", "patches/fix-cdecl.patch", "7ca1cbabe8516152e5d4e5cd5dc7c14b2fd0134f8ad7a8fa64159e07998ebeb4")
+
+    if is_plat("wasm") then
+        add_configs("shared", {description = "Build shared library.", default = false, type = "boolean", readonly = true})
+    end
+
     add_configs("with_zstd", {description = "Enable compression support.", default = false, type = "boolean"})
     add_configs("with_openssl", {description = "Use openssl or bundled sha libraries.", default = false, type = "boolean"})
 
     add_deps("meson", "ninja")
+    if not is_subhost("windows") then
+        add_deps("pkg-config")
+    else
+        add_deps("pkgconf")
+    end
     on_load(function(package)
+        if not package:config("shared") then
+            package:add("defines", "ZCHUNK_STATIC_LIB")
+        end
         if package:config("with_zstd") then
             package:add("deps", "zstd")
         end
@@ -21,7 +35,8 @@ package("libzchunk")
         end
     end)
 
-    on_install(function (package)
+    -- @see https://github.com/zchunk/zchunk/issues/94
+    on_install("!mingw and !msys and !cygwin", function (package)
         local configs = {
             '-Ddocs=false',
             '-Dtests=false',
@@ -33,8 +48,20 @@ package("libzchunk")
 
         io.replace("meson.build", "subdir('src')", "subdir('src/lib')", {plain = true})
         io.replace("meson.build", "not argplib.found()", "false", {plain = true})
+        if is_plat("windows") then
+            -- fix dll name & fix install dir
+            io.replace("src/lib/meson.build", "soversion: so_version,", "install_dir: get_option('libdir'),", {plain = true})
+            io.replace("src/lib/meson.build", "version: meson.project_version(),", "", {plain = true})
+            -- fix lib name
+            io.replace("src/lib/meson.build", "lib_suffix = 'lib'", "", {plain = true})
+        end
 
         import("package.tools.meson").install(package, configs)
+        if is_plat("windows") and not package:config("shared") then
+            -- fix lib name
+            os.cd(package:installdir("lib"))
+            os.mv("libzck.a", "zck.lib")
+        end
     end)
 
     on_test(function (package)
