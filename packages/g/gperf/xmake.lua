f1ba83264062ad14813c5f394c2fd00e655d455d
Redbeanw44602 <redbeana44945@gmail.com>
2025-07-28 01:30:43 +0800

    fcuk 3.3 on windows

--- packages/g/gperf/xmake.lua
+++ packages/g/gperf/xmake.lua
@@ -9,7 +9,6 @@ package("gperf")
 
     add_versions("3.1", "588546b945bba4b70b6a3a616e80b4ab466e3f33024a352fc2198112cdbb3ae2")
     add_versions("3.2.1", "ed5ad317858e0a9badbbada70df40194002e16e8834ac24491307c88f96f9702")
-    add_versions("3.3", "fd87e0aba7e43ae054837afd6cd4db03a3f2693deb3619085e6ed9d8d9604ad8")
 
     if is_host("linux") then
         add_extsources("apt::gperf", "pacman::gperf")
@@ -17,14 +16,6 @@ package("gperf")
 
     on_install("@windows", function (package)
         os.cp("src/config.h.in", "src/config.h")
-        if package:version():ge("3.3") then
-            os.cp("lib/config.h.in", "lib/config.h")
-            os.cp("lib/getopt.in.h", "lib/getopt.h")
-            os.cp("lib/getopt-cdefs.in.h", "lib/getopt-cdefs.h")
-            io.replace("lib/config.h", "if HAVE_STDBOOL_H", "if 1")
-            io.replace("lib/config.h", "#   include <stdbool.h>", "typedef int bool;\n#define false 0\n#define true 1")
-            io.replace("lib/config.h", "#undef GNULIB_XALLOC_DIE", "#define GNULIB_XALLOC_DIE")
-        end
         io.writefile("xmake.lua", [[
             add_rules("mode.debug", "mode.release")
             target("gperf")
@@ -36,10 +27,6 @@ package("gperf")
     end)
 
     on_install("@macosx", "@linux", "@bsd", "@msys", function (package)
-        if package:version():lt("3.3") then
-            io.replace("lib/getline.cc", "register", "", {plain = true})
-            io.replace("lib/getopt.h", "#ifdef __GNU_LIBRARY__", "#if 1", {plain = true})
-        end
         io.replace("lib/getopt.c", "register", "", {plain = true})
         io.replace("lib/getopt.c", "extern char *getenv ();", "#include <stdlib.h>", {plain = true})
         io.replace("lib/getopt.c", "extern int strncmp ();", "#include <string.h>", {plain = true})
