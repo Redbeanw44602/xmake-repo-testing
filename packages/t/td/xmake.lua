e87bfa4ca3cfa96ee8de8c744863100bef75f137
Redbeanw44602 <redbeana44945@gmail.com>
2025-07-27 19:41:30 +0800

    stage.

--- packages/t/td/xmake.lua
+++ packages/t/td/xmake.lua
@@ -25,6 +25,9 @@ package("td")
         if not package:config("shared") then
             package:add("defines", "TDJSON_STATIC_DEFINE")
         end
+        if package:is_cross() then
+            package:add("deps", "tdtl $(version)")
+        end
     end)
 
     on_install(function (package)
@@ -33,6 +36,25 @@ package("td")
         table.insert(configs, "-DTD_INSTALL_STATIC_LIBRARIES=" .. (package:config("shared") and "OFF" or "ON"))
         table.insert(configs, "-DTD_INSTALL_SHARED_LIBRARIES=" .. (package:config("shared") and "ON" or "OFF"))
         table.insert(configs, "-DTD_ENABLE_LTO=" .. (package:config("lto") and "ON" or "OFF"))
+        if package:is_cross() then
+            print("Generate TLO files")
+            os.mkdir("auto")
+            assert(os.exec("tl-parser -e auto/tlo/mtproto_api.tlo scheme/mtproto_api.tl"))
+            assert(os.exec("tl-parser -e auto/tlo/secret_api.tlo scheme/secret_api.tl"))
+            assert(os.exec("tl-parser -e auto/tlo/e2e_api.tlo scheme/e2e_api.tl"))
+            assert(os.exec("tl-parser -e auto/tlo/td_api.tlo scheme/td_api.tl"))
+            assert(os.exec("tl-parser -e auto/tlo/telegram_api.tlo scheme/telegram_api.tl"))
+            print("Generate MTProto API source files")
+            assert(os.exec("generate_mtproto"))
+            print("Generate common TL source files")
+            assert(os.exec("generate_common"))
+            print("Generate JSON TL source files")
+            assert(os.exec("tl_generate_json generate_json"))
+            print("Generate MIME Types source files")
+            assert(os.exec("generate_mime_types_gperf mime_types.txt auto/mime_type_to_extension.gperf auto/extension_to_mime_type.gperf"))
+            assert(os.exec("gperf -m100 --output-file=auto/mime_type_to_extension.cpp auto/mime_type_to_extension.gperf"))
+            assert(os.exec("gperf -m100 --output-file=auto/extension_to_mime_type.cpp auto/extension_to_mime_type.gperf"))
+        end
 
         import("package.tools.cmake").install(package, configs, {target = package:config("shared") and "tdjson" or "tdjson_static"})
     end)
