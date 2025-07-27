a307f732825a752be89b02adb0f215694b9f3499
Redbeanw44602 <redbeana44945@gmail.com>
2025-07-28 00:53:09 +0800

    fix.

--- packages/g/gperf/xmake.lua
+++ packages/g/gperf/xmake.lua
@@ -8,7 +8,6 @@ package("gperf")
              "https://ftp.gnu.org/gnu/gperf/gperf-$(version).tar.gz")
 
     add_versions("3.1", "588546b945bba4b70b6a3a616e80b4ab466e3f33024a352fc2198112cdbb3ae2")
-    add_versions("3.2", "e0ddadebb396906a3e3e4cac2f697c8d6ab92dffa5d365a5bc23c7d41d30ef62")
     add_versions("3.2.1", "ed5ad317858e0a9badbbada70df40194002e16e8834ac24491307c88f96f9702")
     add_versions("3.3", "fd87e0aba7e43ae054837afd6cd4db03a3f2693deb3619085e6ed9d8d9604ad8")
 
@@ -29,11 +28,13 @@ package("gperf")
     end)
 
     on_install("@macosx", "@linux", "@bsd", "@msys", function (package)
-        io.replace("lib/getline.cc", "register", "", {plain = true})
+        if package:version():lt("3.3") then
+            io.replace("lib/getline.cc", "register", "", {plain = true})
+            io.replace("lib/getopt.h", "#ifdef __GNU_LIBRARY__", "#if 1", {plain = true})
+        end
         io.replace("lib/getopt.c", "register", "", {plain = true})
         io.replace("lib/getopt.c", "extern char *getenv ();", "#include <stdlib.h>", {plain = true})
         io.replace("lib/getopt.c", "extern int strncmp ();", "#include <string.h>", {plain = true})
-        io.replace("lib/getopt.h", "#ifdef __GNU_LIBRARY__", "#if 1", {plain = true})
         import("package.tools.autoconf").install(package)
     end)
 
