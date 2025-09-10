34f799033343df7690771c699b87ef22896a1f67
Redbeanw44602 <redbeana44945@gmail.com>
2025-09-10 20:55:22 +0800

    linkgroup

--- packages/o/openssh/xmake.lua
+++ packages/o/openssh/xmake.lua
@@ -143,7 +143,7 @@ package("openssh")
 
         -- fix openssl syslinks on windows.
         if package:is_plat("mingw", "msys", "cygwin") then
-            table.insert(ldflags, "-ladvapi32 -lcrypt32 -luser32 -lws2_32")
+            table.insert(ldflags, "-Wl,--start-group -ladvapi32 -lcrypt32 -luser32 -lws2_32 -Wl,--end-group")
         end
 
         import("package.tools.autoconf").install(package, configs, {ldflags = ldflags})
