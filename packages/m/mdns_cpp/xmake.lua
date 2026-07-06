8adb081b5103af5ae6ed515c2814580318060c47
Redbeanw44602 <redbeana44945@gmail.com>
2026-07-06 12:54:48 +0800

    update

--- packages/m/mdns_cpp/xmake.lua
+++ packages/m/mdns_cpp/xmake.lua
@@ -36,10 +36,8 @@ package("mdns_cpp")
             io.replace("src/mdns.cpp", "sock_addr.sin_addr = in4addr_any;", "sock_addr.sin_addr.s_addr = INADDR_ANY;", {plain = true})
         end
         if package:is_plat("bsd") then
-            print('IS BSD')
             -- To fix incomplete type sockaddr error.
             io.insert("src/utils.cpp", 1, "#include <sys/socket.h>")
-            print(io.readfile('src/utils.cpp'))
         end
 
         local configs = {
