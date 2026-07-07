cd66e1de3284fdfe2c2d4d2afbee1d4037121793
Redbeanw44602 <redbeana44945@gmail.com>
2026-07-07 18:54:23 +0800

    update

--- packages/i/immer/xmake.lua
+++ packages/i/immer/xmake.lua
@@ -15,6 +15,8 @@ package("immer")
     add_deps("cmake")
 
     on_install(function (package)
+        io.replace("cmake/FindBoehmGC.cmake", "include(${CMAKE_ROOT}/Modules/CheckCSourceRuns.cmake)", "include(CheckCSourceRuns)", {plain = true})
+
         local configs = {
             "-Dimmer_BUILD_TESTS=OFF",
             "-Dimmer_BUILD_EXAMPLES=OFF",
